e5a4496f8e54062e5de301f155b6b4353d3db391
Luan <aguirre.luan@escola.pr.gov.br>
2025-10-07 09:34:55 -0300

    07/10

--- Pergunta.js
+++ Pergunta.js
@@ -1,75 +1,75 @@
 criaCartao(
-    'conhecimentos gerais',
+    'Conhecimentos gerais',
     '1. Qual o livro mais vendido no mundo a seguir à Bíblia?',
     'Dom Quixote'
 )
 criaCartao(
-    'geografia',
+    'Geografia',
     '2. Qual a montanha mais alta do Brasil?',
     'Pico da Neblina'
 )
 criaCartao(
-    'astrologia',
+    'Astrologia',
     '3. Quais os planetas do sistema solar?',
     'erra, Vênus, Saturno, Urano, Júpiter, Marte, Netuno, Mercúrio'
 )
 criaCartao(
-    'história',
+    'História',
     '4. Quem foi o primeiro homem a pisar na Lua? Em que ano aconteceu?',
     'Neil Armstrong, em 1969.'
 )
 criaCartao(
-    'química',
+    'Química',
     '5. Qual o metal cujo símbolo químico é o Au?',
     'Ouro'
 )
 criaCartao(
-    'conhecimentos gerais',
+    'Conhecimentos gerais',
     '6. Qual o motivo da condenação de Luís Inácio Lula da Silva em 2018?',
     'Recebimento de apartamento de luxo no Guarujá como propina na Operação Lava Jato'
 )
 criaCartao(
-    'geografia',
+    'Geografia',
     '7. Qual a função da ONU?',
     'Unir as nações com o objetivo de manter a paz e a segurança mundial'
 )
 criaCartao(
-    'história',
+    'História',
     '8. Qual das alternativas contém apenas invenções criadas no Brasil?',
     'Urna eletrônica, soro antiofídico (antídoto contra picada veneno das cobras) e chuveiro elétrico'
 )
 criaCartao(
-    'história',
+    'História',
     '9. Em que país se localizava Auschwitz, o maior campo de concentração nazi?',
     'Polônia'
 )
 criaCartao(
-    'geografia',
+    'Geografia',
     '10. Quais o menor e o maior país do mundo?',
     'Vaticano e Rússia'
 )
 criaCartao(
-    'geografia',
+    'Geografia',
     '11. Que país tem o formato de uma bota?',
     'Itália'
 )
 criaCartao(
-    'ciências',
+    'Ciências',
     '12. Quais são as fases da Lua?',
     'Nova, crescente, cheia e minguante'
 )
 criaCartao(
-    'conhecimentos gerais',
+    'Conhecimentos gerais',
     '13. Quantos andares tem o maior prédio do mundo?',
     '163'
 )
 criaCartao(
-    'geografia',
+    'Geografia',
     '14. Em que região se localiza o estado de Minas Gerais?',
     'Sudeste'
 ) 
 criaCartao(
-    'química',
+    'Química',
     '15. Qual destas substâncias faz parte da composição do vidro?',
     'Areia'
 ) 
